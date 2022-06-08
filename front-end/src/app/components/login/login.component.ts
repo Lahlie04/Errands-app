b54ff6abcb15827303b45a9d4b0446a41f614fb6
Siweprodev <Sanesogiba@gmail.com>
2022-06-08 10:54:41 +0200

    updated code

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormControl,FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -8,14 +8,7 @@ import { FormBuilder,FormControl,FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
-  
-  registerForm: any = new FormGroup({
-   
-    cell_no: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-}) ;
+  constructor() { }
 
   ngOnInit(): void {
   }
