a074da53f6a0ee93e5b691d13e4d107ac0a85911
Lahlie04 <kgomotsomakoti04@gmail.com>
2022-06-08 11:00:30 +0200

    landing page

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { ServiceComponent } from './components/service/service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
